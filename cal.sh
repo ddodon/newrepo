1259d5c41b0786fe58a570e1b0196df6dfdb9d33
ddodon <donms97@naver.com>
2022-04-07 19:24:21 +0900

    cal.sh updated

--- cal.sh
+++ cal.sh
@@ -2,15 +2,18 @@
 
 read num1 < num1.txt
 read num2 < num2.txt
-let Add=$num1+$num2                                                                                                                                          let Sub=$num1-$num2                                                                                                                                          let Mul=$num1*$num2                                                                                                                                          let Div=$num1/$num2 
-
+array=($num1 $num2)
+let Add=${array[0]}+${array[1]}
+let Sub=${array[0]}-${array[1]}
+let Div=${array[0]}/${array[1]}
+let Mul=${array[0]}*${array[1]}
 if [ $# -lt 1 ]; then                                                                                                                                         echo "...none operator parameter...."
  PS3="select menu: "
  select op in "add" "sub" "div" "mul"
  do
     echo
-    echo 'num1: '$num1
-    echo 'num2: '$num2
+    echo 'num1: '${array[0]}
+    echo 'num2: '${array[1]}
     case $op in
     add) echo "op: add"
       echo "result: "$Add;;
@@ -18,7 +21,7 @@ if [ $# -lt 1 ]; then                                                           
      echo "result: "$Sub;;
     div) echo "op: div"
      echo "result: "$Div;;
-    mult) echo "op:mul"
+    mult) echo "op: mul"
      echo "result: "$Mul;;
     esac  
     break
@@ -35,7 +38,7 @@ else
     echo "result: "$Sub;;
   div) echo "op: div"
     echo "result: "$Div;;
-  mul) echo "op:mul"
+  mul) echo "op: mul"
     echo "result: "$Mul;;
 esac
 fi
