093f9c901c5dd79b596105c90480ca2cb0d2c298
ddodon <donms97@naver.com>
2022-04-14 00:45:43 +0900

    assignment#5 isalnum

--- word_freq.py
+++ word_freq.py
@@ -7,15 +7,19 @@ text = open(file,"r")
 num = sys.argv[2] # 상위 몇 개 출력 할지
 dic = {} #dictionary
 
-lines = text.readline()
+lines = text.readlines() #파일을 한 줄 씩 읽어서 리스트에 저장
 
 for line in lines:
-	for word in line.split():
+	words = line.split()
+	for word in words:
+		word = ''.join(char for char in word if char.isalnum()) #문장부호 제거
+		word = word.lower() #소문자
 		if not word in dic:
 			dic[word] = 1
 		else:
 			dic[word] += 1
-result = sorted(dic.item(),key=lambda x: x[1],reverse=True)
 
-print(result[:num])#상위 num개 출력
+for key, ans in dic.items():
+	print("key:",key,"answer:",ans)
 
+#print(result[:num])#상위 num개 출력
