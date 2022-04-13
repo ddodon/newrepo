725adb90858f3d295052e9cb8004cde75ca515b2
ddodon <donms97@naver.com>
2022-04-13 23:22:27 +0900

    assignment#5 param

--- word_freq.py
+++ word_freq.py
@@ -1,6 +1,12 @@
+#!/usr/bin/python
+
 import sys
 
 if __name__=='__main__':
 	print(sys.argv)
-	word = "it is assignment#5"
-	print(word)
+
+word = sys.argv[1]
+num = sys.argv[2]
+
+print(word)
+print(num)
