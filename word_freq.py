b5a4488007dc6b7228f33a218fff88a4edf38b1e
ddodon <donms97@naver.com>
2022-04-13 23:50:51 +0900

    assignment#5 dict

--- word_freq.py
+++ word_freq.py
@@ -5,16 +5,17 @@ import sys
 file = sys.argv[1]
 text = open(file,"r")
 num = sys.argv[2] # 상위 몇 개 출력 할지
-
-word = text.read().split()
-
 dic = {} #dictionary
 
-for w in word:
-	if not w in dic:
-		dic[w] = 1
-	else:
-		dic[w] += 1
+lines = text.readline()
+
+for line in lines:
+	for word in line.split():
+		if not word in dic:
+			dic[word] = 1
+		else:
+			dic[word] += 1
+result = sorted(dic.item(),key=lambda x: x[1],reverse=True)
 
 print(result[:num])#상위 num개 출력
 
